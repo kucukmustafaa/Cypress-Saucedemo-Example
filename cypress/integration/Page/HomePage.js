ed52713216cf68d01090c90dd04f7b162b4b36cb
mustafa küçük <mustafakucukk97@gmail.com>
2022-05-27 22:27:27 +0300

    added login control

--- cypress/integration/Page/HomePage.js
+++ cypress/integration/Page/HomePage.js
@@ -1,9 +1,11 @@
-
+let homePageLogo=".app_logo"
 
 export class HomePage {
 
     verifyUserLogin(){
-
+        cy.url().contains("/inventory.html")
+        cy.get(homePageLogo).should('not.have.css', 'display', 'none')
+        cy.log("home page opened successfully")
     }
 
 
